4de66e8aaad4fdaf1933280eb397f790c3acfa92
Cookiewookie87 <jernej.tratnik@gmail.com>
2018-05-29 15:07:18 +0200

    Init mole game

--- 29 - Countdown Timer/scripts-START.js
+++ 29 - Countdown Timer/scripts-START.js
@@ -1,13 +1,46 @@
 let countdown;
+const displayTimer = document.querySelector(".display__time-left");
+const endTime = document.querySelector(".display__end-time");
+const buttons = document.querySelectorAll("[data-time]");
 
 function timer(seconds) {
+    clearInterval(countdown);
     const now = Date.now();
     const then = now + seconds * 1000;
+    displayTimeLeft(seconds);
+    displayEndTime(then);
     
     countdown = setInterval(function() {
         const secondsLeft = Math.round((then - Date.now()) / 1000);
-        
-        console.log(secondsLeft);
+        if (secondsLeft < 0) {
+            clearInterval(countdown);
+            return;
+        }
+        displayTimeLeft(secondsLeft);
     }, 1000);
 }
+function displayTimeLeft(seconds) {
+    const minutes = Math.floor(seconds / 60);
+    const remainderSeconds = seconds % 60;
+    const display = `${minutes}:${remainderSeconds < 10 ? "0" : ""}${remainderSeconds}`;
+    displayTimer.textContent = display;
+    document.title = display; // to display countdown on title (browser tab)
+}
+function displayEndTime(timeStamp) { // timestamp is the var "const then"
+    const end = new Date(timeStamp);
+    const hour = end.getHours();
+    const minutes = end.getMinutes();
+    endTime.textContent = `Be back at ${hour}:${minutes < 10 ? "0" : ""}${minutes}`;
+}
+function startTimer() {
+    const seconds = parseInt(this.dataset.time); // convert string to number
+    timer(seconds)
+}
 
+buttons.forEach(button => button.addEventListener("click", startTimer));
+document.customForm.addEventListener("submit", function(e) {
+    e.preventDefault();
+    const mins = this.minutes.value;
+    timer(mins * 60);
+    this.reset();
+})
